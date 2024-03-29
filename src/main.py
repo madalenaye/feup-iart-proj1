2896c00e72a264e42a52a071192e5f8a8ace3912
Madalena Ye <mads1040@gmail.com>
2024-03-29 00:45:39 +0000

    start btn

--- src/main.py
+++ src/main.py
@@ -1,25 +1,28 @@
 from board import Board
 import pygame
 from constants import * 
+from button import Button
 
-
+def font(size):
+    return pygame.font.Font('assets/fonts/Tenada.ttf', size)
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     board = Board(screen)
     
-    bg = pygame.image.load('src/images/background.jpeg')
-    bg = pygame.transform.scale(bg, (WIDTH, HEIGHT))
-    
+    bg = pygame.image.load('assets/images/background.jpg')
+    start_button = Button(WIDTH//2 - 150, HEIGHT//2 - 50, pygame.image.load('assets/images/start-btn.png'))
+    start_button.image = pygame.transform.scale(start_button.image, (300, 100))
     pygame.display.set_caption('Main menu')
     
     run = True
     while run:
         screen.blit(bg, (0,0))
+        start_button.draw_button(screen)
         #board.draw_board(screen)
         #board.draw_pieces(screen)
-    
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
